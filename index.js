52b9473907752f14898926b5df538a94e0456a6d
avital <avital@pmax.co>
2021-05-26 13:20:46 +0300

    commit

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ app = express();
 const port = 3000
 const GetRecommendationFunctions = require('./GetRecommnedationFunctions.js');
 const ValidateRecommendationFunctions = require('./RecommendationValidations');
-cron.schedule('0 8 * * SUN', async function() {
-    GetRecommendationFunctions.mainGetRecommndationFunction()
+cron.schedule('0 8 * * SUN', async () => {
+    await GetRecommendationFunctions.mainGetRecommndationFunction()
 });
 
-cron.schedule('0 6 * * SUN', async function() {
+cron.schedule('0 6 * * SUN', async () =>  {
     await ValidateRecommendationFunctions.mainRecommendationsValidation();
 });
 
